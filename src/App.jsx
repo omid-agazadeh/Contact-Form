318d418b9d6afe9534981e50779fb0f9770f2e9f
omid-agazadeh <omidagazadeh.dev@gmail.com>
2024-08-20 14:02:43 +0330

    some style added to inputs

--- src/App.jsx
+++ src/App.jsx
@@ -70,16 +70,34 @@ function App() {
       notReady.map((i) => {
          const setAlert = subList[i].stateFunc;
          setAlert({ ...subList[i].state, status: false });
-      });
+      });   
       if (!notReady.length) {
          setSubmitOk(true);
+         setTimeout(() => {
+            setSubmitOk(false);
+         }, 2000);
       }
       console.log(notReady);
    };
 
    return (
       <>
-         {submitOk ? <div className="bg-red-500 text-center transition-all duration-300 text-4xl pt-12">nice bro ud did it afanar</div> : <div></div>}
+         {submitOk ? (
+            <div className="bg-teal-900 text-white  w-fit text-center transition-all flex flex-col items-start gap-y-2 z-10 duration-300  absolute right-0 mx-auto p-5 rounded-xl left-0 top-[4%]">
+               <span className="text-2xl flex items-center gap-x-2 ">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 20 21">
+                     <path
+                        fill="#fff"
+                        d="M14.28 7.72a.748.748 0 0 1 0 1.06l-5.25 5.25a.748.748 0 0 1-1.06 0l-2.25-2.25a.75.75 0 1 1 1.06-1.06l1.72 1.72 4.72-4.72a.75.75 0 0 1 1.06 0Zm5.47 2.78A9.75 9.75 0 1 1 10 .75a9.76 9.76 0 0 1 9.75 9.75Zm-1.5 0A8.25 8.25 0 1 0 10 18.75a8.26 8.26 0 0 0 8.25-8.25Z"
+                     />
+                  </svg>
+                  Message Sent!
+               </span>
+               <span className="sm:text-lg text-xs">Thanks for completaing the form. we'll be in touch soon!</span>
+            </div>
+         ) : (
+            <div></div>
+         )}
          <div className="md:container flex items-center  absolute top-0 bottom-0 right-0 left-0 justify-center ">
             <div className=" bg-white shadow-lg rounded-xl h-auto my-auto px-10 py-12">
                <h1 className="text-4xl font-semibold ml-1">Contact Us</h1>
@@ -99,7 +117,9 @@ function App() {
                            type="text"
                            id="firstname"
                            value={firstName.value}
-                           className="border-2 p-2 lg:w-64 cursor-pointer hover:border-green-600 transition-all duration-300 rounded-lg border-gray-300 mt-2"
+                           className={`${
+                              !firstName.status ? 'border-red-500 focus:border-red-500 focus:outline-red-500' : 'border-gray-300 focus:outline-green-500 focus:border-green-600 hover:border-green-600 '
+                           } border-2 p-2 lg:w-64 cursor-pointer  transition-all duration-300 rounded-lg  mt-2`}
                         />
                         {!firstName.status && <div className="text-red-600">This field required</div>}
                      </div>
@@ -116,7 +136,9 @@ function App() {
                               }
                            }}
                            type="text"
-                           className="border-2 p-2 lg:w-64 rounded-lg hover:border-green-600 transition-all duration-300 border-gray-300 mt-2"
+                           className={` ${
+                              !lastName.status ? 'border-red-500 focus:border-red-500 focus:outline-red-500' : 'border-gray-300 focus:outline-green-500 focus:border-green-600 hover:border-green-600 '
+                           } border-2 p-2 lg:w-64 rounded-lg  transition-all duration-300  mt-2`}
                         />
                         {!lastName.status && <div className="text-red-600">This field required</div>}
                      </div>
@@ -134,9 +156,17 @@ function App() {
                            }
                         }}
                         type="text"
-                        className="border-2 p-2 w-full rounded-lg hover:border-green-600 transition-all duration-300 cursor-pointer border-gray-300 mt-2"
+                        className={` ${
+                           !email.status ? 'border-red-500 focus:border-red-500 focus:outline-red-500' : 'border-gray-300 focus:outline-green-500 focus:border-green-600 hover:border-green-600 '
+                        } border-2 p-2 w-full rounded-lg  transition-all duration-300  mt-2`}
                      />
-                     {!email.status && <div className="text-red-600">Please enter a valid email address</div>}
+                     {!email.status && (
+                        <div className="text-red-600">
+                           Please enter a valid email address
+                           <br />
+                           for example example@gmail.com
+                        </div>
+                     )}
                   </div>
                   <div className="">
                      <label>Quary Type</label>
@@ -146,9 +176,9 @@ function App() {
                               onClick={(e) => {
                                  const checkbox = e.target.checked;
                                  if (checkbox) {
-                                    setQueryType({ value: true, status: false, statusSubmit: true });
+                                    setQueryType({ value: true, status: true, statusSubmit: true });
                                  } else {
-                                    setQueryType({ value: false, status: true, statusSubmit: false });
+                                    setQueryType({ value: false, status: false, statusSubmit: false });
                                  }
                               }}
                               type="radio"
@@ -160,14 +190,16 @@ function App() {
                               Genral Enquiry
                            </label>
                         </div>
-                        <div className="flex gap-x-3 border-2 py-2 px-4 w-1/2 rounded-lg hover:border-green-600  border-gray-300 mt-2 transition-all duration-300 has-[:checked]:bg-green-500/25 has-[:checked]:border-green-500">
+                        <div
+                           className={`flex gap-x-3 border-2 py-2 px-4 w-1/2 rounded-lg hover:border-green-600  border-gray-300  mt-2 transition-all duration-300 has-[:checked]:bg-green-500/25 has-[:checked]:border-green-500 `}
+                        >
                            <input
                               onClick={(e) => {
                                  const checkbox = e.target.checked;
                                  if (checkbox) {
-                                    setQueryType({ value: false, status: true, statusSubmit: true });
+                                    setQueryType({ status: true, statusSubmit: true });
                                  } else {
-                                    setQueryType({ value: true, status: false, statusSubmit: false });
+                                    setQueryType({ status: false, statusSubmit: false });
                                  }
                               }}
                               type="radio"
@@ -199,7 +231,9 @@ function App() {
                         id="textarea"
                         rows="4"
                         cols="50"
-                        className="w-full border-2 hover:border-green-600 transition-all cursor-pointer duration-300 rounded-lg p-3 resize-none"
+                        className={`w-full ${
+                           !message.status ? 'border-red-500 focus:border-red-500 focus:outline-red-500' : 'border-gray-300 focus:outline-green-500 focus:border-green-600 hover:border-green-600 '
+                        } border-2 transition-all cursor-pointer duration-300 rounded-lg p-3 resize-none`}
                      ></textarea>
                      {!message.status && <div className="text-red-600">This field required</div>}
                   </div>
@@ -218,7 +252,9 @@ function App() {
                            id="rule"
                            className="accent-green-600 sm:w-4 w-3 cursor-pointer"
                         />
-                        <label htmlFor="rule" className='sm:text-base text-xs cursor-pointer'>i consent to being contacted by the team</label>
+                        <label htmlFor="rule" className="sm:text-base text-xs cursor-pointer">
+                           i consent to being contacted by the team
+                        </label>
                      </div>
                      {!rule.status && <span className="text-red-600 sm:text-base text-xs">To submit rhis form,please consent to being contacked</span>}
                   </div>
